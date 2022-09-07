500cd89cce918000cb77638a5748c137be75bcb2
marian <noaghea.marian-florentin@keysight.com>
2022-09-07 12:41:14 +0300

    typo in script fixed

--- kubelet-plugin/build-plugin2-locally-and-place-it.sh
+++ kubelet-plugin/build-plugin2-locally-and-place-it.sh
@@ -2,7 +2,7 @@
 
 cd ~/kubernetes;
 make plugin WHAT=cmd/kubelet/demo2-plugins --env CGO_ENABLED=1 --env GO111MODULES=on GOFLAGS=-v
-sudo mv plugin1.so /etc/kubernetes/cci
+sudo mv plugin2.so /etc/kubernetes/cci
 echo 'Plugin was built right now, see below in /etc/kubernetes/cci';
 ls -lrt /etc/kubernetes/cci | grep plugin2.so;
 cd ~/k8s-tools/kubelet-plugin
